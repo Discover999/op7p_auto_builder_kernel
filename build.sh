35e590c29e7918d8473f3511e802f9895f566979
Discover999 <32007383+Discover999@users.noreply.github.com>
2025-05-20 12:39:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ echo "⭐$KBUILD_BUILD_HOST"
 
 echo "🌌Configure kernel..."
 # Configure kernel     
-make O=out ARCH=arm64 sm8150-perf_defconfig
+make O=out ARCH=arm64 sm8150_defconfig
 yes "" | make O=out ARCH=arm64 olddefconfig
 
 
